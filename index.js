8caa27cf553939825c9e025979232af2a6150974
Mathis <mathis.pati@gmail.com>
2021-04-22 22:00:39 +0200

    allow cors origin to poulepoule online website

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(router);
 
 const io = require('socket.io')(http, {
     cors: {
-        origin: 'http://127.0.0.1:5500'
+        origin: ['http://127.0.0.1:5500', 'https://poulepoule-online.herokuapp.com/']
     }
 });
 
